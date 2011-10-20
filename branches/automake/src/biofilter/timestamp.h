eee083c2a51059afc1df31f4e56e260b23f38ec7
atf3 <atf3@psu.edu>
2011-10-20 13:31:03 +0000

    build 30
    git-svn-id: https://svn.ritchielab.psu.edu/svn/projects/biofilter@1369 9e62ecf6-770d-0410-a9d7-e62a33e3d092

--- branches/automake/src/biofilter/timestamp.h
+++ branches/automake/src/biofilter/timestamp.h
@@ -1,13 +1,11 @@
 #ifndef TIMESTAMP_H
 #define TIMESTAMP_H
 
-
 #define APPMAJOR 1
 #define APPMINOR 0
 #define APPBUGFIX 0
-
-#define BUILD_NUMBER 28
-#define BUILD_DATE "Wed Oct 19 9:15:00 EDT 2011"
+#define BUILD_NUMBER 29
+#define BUILD_DATE "Thu Oct 20 9:30:00 EDT 2011"
 
 #ifdef DEBUG
 #define BUILD_TYPE "Debug"
