d5c3f9d67e7c82da45537b980d2d4939d1d0af52
atf3 <atf3@psu.edu>
2011-10-26 15:07:23 +0000

    - copy eric's r993 src to biofilter 0.5.0 tag
    git-svn-id: https://svn.ritchielab.psu.edu/svn/projects/biofilter@1381 9e62ecf6-770d-0410-a9d7-e62a33e3d092

---
+++ tags/0.5.0/src/biofilter/timestamp.h
@@ -0,0 +1,18 @@
+#ifndef TIMESTAMP_H
+#define TIMESTAMP_H
+
+
+#define APPMAJOR 0
+#define APPMINOR 5
+#define APPBUGFIX 0
+
+#define BUILD_NUMBER 1142
+#define BUILD_DATE "Fri May 28 14:25:28 CDT 2010"
+
+#ifdef DEBUG
+#define BUILD_TYPE "Debug"
+#else
+#define BUILD_TYPE ""
+#endif
+
+#endif
