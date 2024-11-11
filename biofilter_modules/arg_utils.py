9517edb100e211c0f1430f9ef6ac70da17d60de7
Andre <ricoandre@hotmail.com>
2024-11-11 09:34:20 -0500

    Tests to arg_utils

--- biofilter_modules/arg_utils.py
+++ biofilter_modules/arg_utils.py
@@ -56,10 +56,19 @@ class cfDialect(csv.Dialect):
 
 def parseCFile(cfName, parser, options):
     """
-    Recursively parses configuration files, supports 'include' directives.
+    This function reads and interprets a configuration file, processing
+    command-line options and allowing the inclusion of other configuration
+    files. It is used to configure arguments flexibly, supporting
+    configuration files with recursive references to other configuration files.
     """
+
+    # 1. Control of recursion and prevention of cycles:
+    # cfStack keep track of the configuration files being processed to avoid
+    # cycles.
     cfStack = []
     cfAbs = "<stdin>" if cfName == "-" else os.path.abspath(cfName)
+    # Check if the current file i(cfAbs) s already in the stack, which would
+    # indicate a cycle. If so, the program exits with an error message.
     if cfAbs in cfStack:
         sys.exit(
             "ERROR: configuration files include each other in a loop! %s"
@@ -67,30 +76,48 @@ def parseCFile(cfName, parser, options):
         )
     cfStack.append(cfAbs)
 
-    # Set up iterators
+    # 2. Reading the configuration file:
+    # cfHandle: Opens the file specified by cfName, or uses sys.stdin if
+    # cfName is "-".
     cfHandle = sys.stdin if cfName == "-" else open(cfName, "r")
+    # cfStream: Removes extra tabs and spaces from the file lines.
     cfStream = (line.replace("\t", " ").strip() for line in cfHandle)
+    # cfLines: Filters out empty lines and comments (lines starting with #).
     cfLines = (line for line in cfStream if line and not line.startswith("#"))
+    # cfReader: Uses csv.reader to interpret the file, allowing it to read
+    # arguments split by spaces, respecting quotes to preserve substrings
+    # (using cfDialect
     cfReader = csv.reader(cfLines, dialect=cfDialect)
 
-    # Parse the file; recurse for includes, store the rest
+    # 3. Parsing the configuration file:
     cfArgs = []
     for line in cfReader:
+        # To each line of the configuration file, -- is added to the beginning
+        # of each argument so that it is interpreted as a command-line argument
         line[0] = "--" + line[0].lower().replace("_", "-")
+        # If the argument is --include, the function calls itself recursively
+        # with the included file (defined by line[ln]), allowing the nesting of
+        # configuration files.
         if line[0] == "--include":
             for ln in range(1, len(line)):
                 parseCFile(line[ln], parser, options)
+        # Otherwise, the arguments are added to cfArgs, and --end-of-line is
+        # inserted to mark the end of a line
         else:
             cfArgs.extend(line)
             cfArgs.append("--end-of-line")
 
-    # Close the stream and parse the args
+    # 4. Closing the configuration file and parsing the arguments:
+    # After processing the configuration file, it is closed if it is not stdin.
     if cfHandle != sys.stdin:
         cfHandle.close()
+    # parser_args is called in the parser with the extracted arguments, and
+    # options is filled with the values.
     parser.parse_args(args=cfArgs, namespace=options)
     if options.configuration:
         raise Exception(
             "unexpected argument(s): %s" % " ".join(options.configuration)
         )  # noqa: E501
-
+    # Finally, the current file is removed from cfStack, signaling the end of
+    # its processing.
     cfStack.pop()
