3628fb740da66e9d0ab52dbe4fed9d5309bda2fd
Andre Luis Garao Rico <alrico@superman.pmacs.upenn.edu>
2025-06-10 09:22:44 -0400

    jobs chrm 1

--- scripts/jobs/load_dbsnp.py
+++ scripts/jobs/load_dbsnp.py
@@ -1,30 +1,17 @@
 from biofilter import Biofilter
 
-db_uri = "sqlite:///dev_biofilter.db"
+db_uri = "sqlite:///dev_biofilter_2.db"
 
 if __name__ == "__main__":
     bf = Biofilter(db_uri)
 
     # ETL HGNC -- GENES
-    # bf.update(data_sources=["dbsnp_chr1"], run_steps=["extract"], force_steps=["extract"])
-    bf.update(data_sources=["dbsnp_chr1"], run_steps=["transform"], force_steps=["transform"])
-    bf.update(data_sources=["dbsnp_chr1"], run_steps=["load"], force_steps=["load"])
+    # bf.update(data_sources=["dbsnp_chr1"], run_steps=["transform"], force_steps=["transform"])
+    # bf.update(data_sources=["dbsnp_chr1"], run_steps=["load"], force_steps=["load"])
 
-    # bf.update(data_sources=["dbsnp_chr2"], run_steps=["extract"], force_steps=["extract"])
+    bf.update(data_sources=["dbsnp_chr2"], run_steps=["extract"], force_steps=["extract"])
     # bf.update(data_sources=["dbsnp_chr2"], run_steps=["transform"], force_steps=["transform"])
     # bf.update(data_sources=["dbsnp_chr2"], run_steps=["load"], force_steps=["load"])
-
-    # bf.update(data_sources=["dbsnp_chr3"], run_steps=["extract"], force_steps=["extract"])
-    # bf.update(data_sources=["dbsnp_chr3"], run_steps=["transform"], force_steps=["transform"])
-    # bf.update(data_sources=["dbsnp_chr3"], run_steps=["load"], force_steps=["load"])
-
-    # bf.update(data_sources=["dbsnp_chr4"], run_steps=["extract"], force_steps=["extract"])
-    # bf.update(data_sources=["dbsnp_chr4"], run_steps=["transform"], force_steps=["transform"])
-    # bf.update(data_sources=["dbsnp_chr4"], run_steps=["load"], force_steps=["load"])
-
-    # bf.update(data_sources=["dbsnp_chr5"], run_steps=["extract"], force_steps=["extract"])
-    # bf.update(data_sources=["dbsnp_chr5"], run_steps=["transform"], force_steps=["transform"])
-    # bf.update(data_sources=["dbsnp_chr5"], run_steps=["load"], force_steps=["load"])
 
     # bf.update(data_sources=["dbsnp_chr6"], run_steps=["extract"], force_steps=["extract"])
     # bf.update(data_sources=["dbsnp_chr6"], run_steps=["transform"], force_steps=["transform"])
