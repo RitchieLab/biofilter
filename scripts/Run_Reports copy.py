1868110a9261b7dffb3c517df55bde389dfb6e6f
AndreRico <ricoandre@hotmail.com>
2025-10-01 15:57:30 +0200

    sync

--- scripts/Run_Reports copy.py
+++ scripts/Run_Reports copy.py
@@ -6,34 +6,37 @@ db_uri = "postgresql+psycopg2://bioadmin:bioadmin@localhost/biofilter"
 if __name__ == "__main__":
 
     bf = Biofilter(db_uri)
-
     print(bf.metadata.schema_version)
 
-    # List reports
-    # print(bf.report.list_reports())
+    bf.report.list_reports()
 
-    # Run specific report
-    # df = bf.report.run_report("report_etl_status")
+    bf.report.explain("report_gene_to_snp")
 
-    # result = bf.report.run_report(
-    #     "report_gene_to_snp",
-    #     assembly='38',
-    #     input_data=[
-    #         "TXLNGY",
-    #         "HGNC:18473",
-    #         "246126",
-    #         "ENSG00000131002",
-    #         "HGNC:5"
-    #     ],
-    # )
+    result = bf.report.run_example_report("report_gene_to_snp")
+
     result = bf.report.run_report(
         "report_gene_to_snp",
         input_data="/home/bioadmin/biofilter/scripts/gene_list.txt"
     )
 
-    bf.report.explain("report_gene_to_snp")
-    # result = bf.report.run_example_report("report_gene_to_snp")
-    # bf.report.list_reports()
+    result = bf.report.run_report(
+        "report_gene_to_snp",
+        assembly='38',
+        input_data=[
+            "TXLNGY",
+            "HGNC:18473",
+            "246126",
+            "ENSG00000131002",
+            "HGNC:5"
+        ],
+    )
+    
+    
+    # List reports
+    # print(bf.report.list_reports())
+
+    # Run specific report
+    # df = bf.report.run_report("report_etl_status")
 
     # def flatten_allele(val):
     #     if isinstance(val, list):
