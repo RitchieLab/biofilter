6f711163f2d0971500db1dea589f762b00161d10
Andre <ricoandre@hotmail.com>
2025-10-01 07:19:42 -0400

    Test remote Conections

--- scripts/Run_Reports.py
+++ scripts/Run_Reports.py
@@ -1,7 +1,18 @@
 from biofilter import Biofilter
 
+
+# from urllib.parse import quote_plus
+# user = "bioadmin"
+# pwd  = quote_plus("xxx@1111")  # -> 'xxx%401111'
+# host = "<SERVER_IP>"
+# uri  = f"postgresql+psycopg2://{user}:{pwd}@{host}:5432/biofilter?sslmode=require"
+# # engine = create_engine(uri)
+
+
 # db_uri = "sqlite:///dev_biofilter.db"
 db_uri = "postgresql+psycopg2://bioadmin:bioadmin@localhost/biofilter"
+# db_uri = "postgresql+psycopg2://bioadmin:Penn%402025@109.199.114.191:5432/biofilter?sslmode=require"
+
 
 if __name__ == "__main__":
 
@@ -10,30 +21,22 @@ if __name__ == "__main__":
     print(bf.metadata.schema_version)
 
     # List reports
-    # print(bf.report.list_reports())
+    print(bf.report.list_reports())
 
     # Run specific report
     # df = bf.report.run_report("report_etl_status")
 
-    # result = bf.report.run_report(
-    #     "report_gene_to_snp",
-    #     assembly='38',
-    #     input_data=[
-    #         "TXLNGY",
-    #         "HGNC:18473",
-    #         "246126",
-    #         "ENSG00000131002",
-    #         "HGNC:5"
-    #     ],
-    # )
     result = bf.report.run_report(
         "report_gene_to_snp",
-        input_data="/home/bioadmin/biofilter/scripts/gene_list.txt"
+        assembly='38',
+        input_data=[
+            "TXLNGY",
+            "HGNC:18473",
+            "246126",
+            "ENSG00000131002",
+            "HGNC:5"
+        ],
     )
-
-    # bf.report.explain("report_gene_to_snp")
-    # result = bf.report.run_example_report("report_gene_to_snp")
-    # bf.report.list_reports()
 
     # def flatten_allele(val):
     #     if isinstance(val, list):
@@ -44,4 +47,4 @@ if __name__ == "__main__":
     # result["Alt Allele"] = result["Alt Allele"].apply(flatten_allele)
 
 
-    print(result)
+    # print(result)
