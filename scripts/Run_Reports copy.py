d204df4db28857f1cae3c6157638723f03934333
AndreRico <ricoandre@hotmail.com>
2025-10-01 13:26:50 +0200

    sync chrom 2 load script

---
+++ scripts/Run_Reports copy.py
@@ -0,0 +1,47 @@
+from biofilter import Biofilter
+
+# db_uri = "sqlite:///dev_biofilter.db"
+db_uri = "postgresql+psycopg2://bioadmin:bioadmin@localhost/biofilter"
+
+if __name__ == "__main__":
+
+    bf = Biofilter(db_uri)
+
+    print(bf.metadata.schema_version)
+
+    # List reports
+    # print(bf.report.list_reports())
+
+    # Run specific report
+    # df = bf.report.run_report("report_etl_status")
+
+    # result = bf.report.run_report(
+    #     "report_gene_to_snp",
+    #     assembly='38',
+    #     input_data=[
+    #         "TXLNGY",
+    #         "HGNC:18473",
+    #         "246126",
+    #         "ENSG00000131002",
+    #         "HGNC:5"
+    #     ],
+    # )
+    result = bf.report.run_report(
+        "report_gene_to_snp",
+        input_data="/home/bioadmin/biofilter/scripts/gene_list.txt"
+    )
+
+    bf.report.explain("report_gene_to_snp")
+    # result = bf.report.run_example_report("report_gene_to_snp")
+    # bf.report.list_reports()
+
+    # def flatten_allele(val):
+    #     if isinstance(val, list):
+    #         return ";".join(val)
+    #     return val
+
+    # result["Ref Allele"] = result["Ref Allele"].apply(flatten_allele)
+    # result["Alt Allele"] = result["Alt Allele"].apply(flatten_allele)
+
+
+    print(result)
---
+++ scripts/carga_chr2.py
@@ -0,0 +1,109 @@
+from biofilter import Biofilter
+
+# db_uri = "sqlite:///biofilter.db"
+db_uri = "postgresql+psycopg2://bioadmin:bioadmin@localhost/biofilter"
+
+# Configure below
+data_sources_to_process = [
+    # Genes
+    # -----
+    # "hgnc",
+    # "gene_ncbi",
+    # "ensembl",
+    # "gene_ncbi",
+    #
+    # Proteins
+    # --------
+    # "pfam",
+    # "uniprot",
+    #
+    # Pathways
+    # --------
+    # "reactome",
+    # "kegg_pathways",
+    #
+    # Gene Ontology
+    # -------------
+    # "gene_ontology",
+    #
+    # Variants
+    # --------
+    # "dbsnp_sample",
+    # "dbsnp_chr1",
+    "dbsnp_chr2",
+    # "dbsnp_chr3",
+    # "dbsnp_chr4",
+    # "dbsnp_chr5",
+    # "dbsnp_chr6",
+    # "dbsnp_chr7",
+    # "dbsnp_chr8",
+    # "dbsnp_chr9",
+    # "dbsnp_chr10",
+    # "dbsnp_chr11",
+    # "dbsnp_chr12",
+    # "dbsnp_chr13",
+    # "dbsnp_chr14",
+    # "dbsnp_chr15",
+    # "dbsnp_chr16",
+    # "dbsnp_chr17",
+    # "dbsnp_chr18",
+    # "dbsnp_chr19",
+    # "dbsnp_chr20",
+    # "dbsnp_chr21",
+    # "dbsnp_chr22",
+    # "dbsnp_chrx",
+    # "dbsnp_chry",
+    # "dbsnp_chrmt",
+    # "gwas",
+    #
+    # RelationShips
+    # -------------
+    # "reactome_relationships",
+    # "uniprot_relationships",
+    #
+    # DISEASE
+    # -------
+    # "mondo",
+    # "mondo_relationships",
+    #
+    # CHEMICAL
+    # --------
+    # "chebi",
+]
+
+run_steps = [
+    "extract",
+    "transform",
+    "load",
+    # "all"
+]  # noqa E501
+
+if __name__ == "__main__":
+    bf = Biofilter(db_uri, debug_mode=True)
+    # bf = Biofilter(db_uri)
+
+    for source in data_sources_to_process:
+        for step in run_steps:
+            if step != "all":
+                try:
+                    print(f"▶ Running ETL - Source: {source} | Step: {step}")
+                    bf.update(
+                        data_sources=[source],
+                        run_steps=[step],
+                        force_steps=[step],
+                    )
+                except Exception as e:
+                    print(f"❌ Error processing {source} [{step}]: {e}")
+            elif step == "all":
+                try:
+                    print(f"▶ Running ETL - Source: {source} | Step: {step}")
+                    bf.update(
+                        data_sources=[source],
+                        # run_steps=[step],
+                        # force_steps=[step],
+                    )
+                except Exception as e:
+                    print(f"❌ Error processing {source} [{step}]: {e}")
+
+    print("✅ All ETL tasks finished.")
+    print("------------------------------")
