124ac41a91b6f9189a62c966f96772dae757d0ca
Andre <ricoandre@hotmail.com>
2026-01-13 10:10:39 -0500

    Docs Settings

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -14,11 +14,9 @@ release = '4.0.0'
 # -- General configuration ---------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
 
-extensions = []
 
 templates_path = ['_templates']
 exclude_patterns = []
-
 
 
 # -- Options for HTML output -------------------------------------------------
