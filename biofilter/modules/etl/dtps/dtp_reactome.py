f88d8c1a195454d424db8df48569488971ecd63e
Andre <ricoandre@hotmail.com>
2026-04-28 18:28:43 -0400

    Improve performance DTP

--- biofilter/modules/etl/dtps/dtp_reactome.py
+++ biofilter/modules/etl/dtps/dtp_reactome.py
@@ -30,8 +30,8 @@ class DTP(DTPBase, EntityQueryMixin):
         # DTP versioning
         self.dtp_name = "dtp_reactome"
         self.dtp_version = "1.1.0"
-        self.compatible_schema_min = "0.0.0"
-        self.compatible_schema_max = "4.0.0"
+        self.compatible_schema_min = "4.0.0"
+        self.compatible_schema_max = "5.0.0"
 
     # -------------------------------------------------------------------------
     #                            EXTRACT METHOD
